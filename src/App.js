17dc513ea2ecdc1ae54c5655f89121ba34c5baae
Ricardo Augusto <166884977+RicardoAugust-0@users.noreply.github.com>
2025-05-15 17:23:25 -0300

    `Fixed typo in logo import path in App.js`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Logo from './components/logo'
+import Logo from './components/Logo'
 import profile_Icon from './imgs/perfil.svg';
 import bag from './imgs/sacola.svg';
 
